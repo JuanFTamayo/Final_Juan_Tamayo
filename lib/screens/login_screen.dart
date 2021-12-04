945fba7fb7cdf9d46ea89133b39a9933b1711424
JuanFTamayo <88947668+JuanFTamayo@users.noreply.github.com>
2021-12-04 12:58:00 -0500

    view

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:final_juan_tamayo/components/loader_component.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({ Key? key }) : super(key: key);
@@ -10,20 +11,22 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   bool _loader= false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       backgroundColor: Color(0xFFFFEB3B),
+       backgroundColor: Color(0xFF51E1ED),
       body: Stack(
         children: <Widget>[
           SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                SizedBox(height: 40,),
+                SizedBox(height: 120,),
                 _showLogo(),
                 SizedBox(height: 20,),
-                _showButton(),
+                _showButton()
+                
               ],
             ),
           ),
@@ -34,7 +37,7 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  _showLogo() {
+  Widget _showLogo() {
      return Image(
       image: AssetImage('assets/Logo.png'),
       width: 300,
@@ -42,7 +45,7 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  _showButton() {
+  Widget _showButton() {
      return Container(
       margin: EdgeInsets.only(left: 10, right: 10),
       child: Column(
@@ -55,5 +58,27 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  _showGoogleLoginButton() {}
+  _showGoogleLoginButton() {
+
+     return Row(
+      children: <Widget>[
+        Expanded(
+          child: ElevatedButton.icon(
+            onPressed: (){}, 
+            icon: FaIcon(
+              FontAwesomeIcons.google,
+              color: Colors.red,
+            ), 
+            label: Text('Iniciar sesión con Google'),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.white,
+              onPrimary: Colors.black
+            )
+          )
+        )
+      ],
+    );
+  }
+
+  _loginGoogle() {}
 }
